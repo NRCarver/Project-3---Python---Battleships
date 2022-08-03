61f3c0a9be3f26463698bb9154d34de0210381ec
NRCarver <carver1994@hotmail.com>
2022-08-03 17:01:18 +0000

    Add computer win conditions and display computer win

--- run.py
+++ run.py
@@ -15,7 +15,7 @@ while True:
 
 
 while True:
-    board_size = int(input("What size board would you like to play? "))
+    board_size = int(input("What width board would you like to play? "))
     if board_size < 3 or board_size > 7:
         print("The board size needs to be between 3x3 and 7x7")
     else:
@@ -71,8 +71,12 @@ def computer_guess():
     computer_guess_col = fcomputer_guess_col(player_board)
     while player_board[player_ship_row][player_ship_col] == "O":
         if computer_guess_row == player_ship_row and computer_guess_col == player_ship_col:
-            print("Computer hit the battleship!")
+            print(f"Computer guessed ({computer_guess_row},{computer_guess_col}) and hit the battleship!")
             player_board[computer_guess_row][computer_guess_col] = "X"
+            print_player_board(player_board)
+            break
+        elif player_board[computer_guess_row][computer_guess_col] == "-":
+            computer_guess()
         else:
             print(f"Computer guessed ({computer_guess_row},{computer_guess_col}) and missed!")
             print(player_ship_row, player_ship_col)
@@ -89,24 +93,29 @@ computer_ship_col = rand_col_computer(player_board)
 # Game logic to check if a guess is valid, miss or hit
 
 while computer_board[computer_ship_row][computer_ship_col] == "O":
-    guess_row = int(input("Guess the Row: "))
-    guess_col = int(input("Guess the Column: "))
-
-    if guess_row == computer_ship_row and guess_col == computer_ship_col:
-        print("You hit the battleship!")
-        computer_board[guess_row][guess_col] = "X"
+    if player_board[player_ship_row][player_ship_col] == "X":
+        print("You lost the game!")
+        break
     else:
-        if guess_row > int(board_size - 1) or guess_col > int(board_size - 1):
-            print("invalid guess")
-        elif computer_board[guess_row][guess_col] == "-":
-            print("You have already guessed here, try again")
-        else: 
-            print("You missed!")
-            print(computer_ship_row, computer_ship_col)
-            computer_board[guess_row][guess_col] = "-"
-            computer_guess()
-            print_computer_board(computer_board)
+        guess_row = int(input("Guess the Row: "))
+        guess_col = int(input("Guess the Column: "))
+        if guess_row == computer_ship_row and guess_col == computer_ship_col:
+            print("You hit the battleship!")
+            computer_board[guess_row][guess_col] = "X"
+        else:
+            if guess_row > int(board_size - 1) or guess_col > int(board_size - 1):
+                print("invalid guess")
+            elif computer_board[guess_row][guess_col] == "-":
+                print("You have already guessed here, try again")
+            else: 
+                print(f"You guessed ({guess_row},{guess_col}) and you missed!")
+                print(computer_ship_row, computer_ship_col)
+                computer_board[guess_row][guess_col] = "-"
+                computer_guess()
+                print_computer_board(computer_board)
 else:
     print_player_board(player_board)
     print_computer_board(computer_board)
     print(f"You sunk the battleship! Well done {name}!")
+
+print("Game Over, would you like to play again?")
