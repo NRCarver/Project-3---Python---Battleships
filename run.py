43da677eb342554b4bd1fa87dcb5606ed1926aa7
NRCarver <carver1994@hotmail.com>
2022-08-03 11:20:10 +0000

    Create loop till game and and win condition

--- run.py
+++ run.py
@@ -30,16 +30,22 @@ def rand_col(board):
 ship_row = rand_row(board)
 ship_col = rand_col(board)
 
-guess_row = int(input("Guess the Row: "))
-guess_col = int(input("Guess the Column: "))
+while board[ship_row][ship_col] == "O":
+    guess_row = int(input("Guess the Row: "))
+    guess_col = int(input("Guess the Column: "))
 
-if guess_row == ship_row and guess_col == ship_col:
-    print("You hit the battleship!")
-else:
-    if guess_row > int(6) or guess_col > int(6):
-        print("invalid guess")
-    else: 
-        print("You missed!")
-        print(ship_row, ship_col)
+    if guess_row == ship_row and guess_col == ship_col:
+        print("You hit the battleship!")
         board[guess_row][guess_col] = "X"
-        print_board(board)
+    else:
+        if guess_row > int(6) or guess_col > int(6):
+            print("invalid guess")
+        elif board[guess_row][guess_col] == "X":
+            print("You have already guessed here")
+        else: 
+            print("You missed!")
+            print(ship_row, ship_col)
+            board[guess_row][guess_col] = "X"
+            print_board(board)
+else:
+    print("You sunk the battleship! Well done!")
