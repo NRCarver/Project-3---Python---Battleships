bb098f202f9578a7dfb2fb13b685e061fedca375
NRCarver <carver1994@hotmail.com>
2022-08-03 22:09:12 +0000

    Add comments to code and slightly edit game text

--- run.py
+++ run.py
@@ -6,6 +6,8 @@ while True:
     player_board = []
     computer_board = []
 
+    # Enter your name
+
     while True:
         name_entry = input("What is your name? ")
         name = name_entry.capitalize()
@@ -14,7 +16,7 @@ while True:
         else:
             break
 
-
+    # Choose the size of board you want to play on.
     while True:
         try:
             board_size = int(input("What width board would you like to play? "))
@@ -25,12 +27,15 @@ while True:
         except ValueError:
             print("Please enter an integer between 3 and 7")
 
+    # Create your board and the computer board
+
     for i in range(0, board_size):
         player_board.append(["O"] * board_size)
 
     for i in range(0, board_size):
         computer_board.append(["O"] * board_size)
 
+    # Functions to print your board and the computer board
 
     def print_player_board(player_board):
         print(f"{name}'s Board")
@@ -87,13 +92,15 @@ while True:
                 print_player_board(player_board)
                 break
         
+    # Create coordinates for player ships and computer ships
 
     player_ship_row = rand_row_computer(computer_board)
     player_ship_col = rand_col_computer(computer_board)
 
     computer_ship_row = rand_row_computer(player_board)
     computer_ship_col = rand_col_computer(player_board)
-    # Game logic to check if a guess is valid, miss or hit
+
+    # Game logic to check if a guess is valid, miss or hit and end game
 
     while computer_board[computer_ship_row][computer_ship_col] == "O":
         if player_board[player_ship_row][player_ship_col] == "X":
@@ -124,7 +131,9 @@ while True:
         print_computer_board(computer_board)
         print(f"You sunk the battleship! Well done {name}!")
 
-    restart = input("Game Over, would you like to play again? Enter 'Y' to restart ")
+    # Restart the game or not
+
+    restart = input("Game Over, would you like to play again or close the game? Enter 'Y' to restart ")
 
     if restart != "Y":
         break
