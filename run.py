a20a3e8ed2fa454e2952f7d652a7bbcdf1428999
NRCarver <carver1994@hotmail.com>
2022-08-03 23:48:40 +0000

    Refactor code to pass Pep8 check

--- run.py
+++ run.py
@@ -19,7 +19,7 @@ while True:
     # Choose the size of board you want to play on.
     while True:
         try:
-            board_size = int(input("What width board would you like to play? "))
+            board_size = int(input("What board size would you like to play? "))
             if board_size < 3 or board_size > 7:
                 print("The board size needs to be between 3x3 and 7x7")
             else:
@@ -47,17 +47,14 @@ while True:
         for row in computer_board:
             print(" ".join(row))
 
-
-
     print_player_board(player_board)
     print("\n")
     print_computer_board(computer_board)
 
-
     # create random ship placement
 
-    ships_num = board_size
-
+    ships_num = (board_size - 2)
+    print(ships_num)
 
     def rand_row_computer(computer_board):
         return randint(0, len(computer_board) - 1)
@@ -71,27 +68,29 @@ while True:
     def fcomputer_guess_col(player_board):
         return randint(0, len(player_board) - 1)
 
-
     # Function for computer guesses
 
     def computer_guess():
         computer_guess_row = fcomputer_guess_row(player_board)
         computer_guess_col = fcomputer_guess_col(player_board)
         while player_board[player_ship_row][player_ship_col] == "O":
-            if computer_guess_row == player_ship_row and computer_guess_col == player_ship_col:
-                print(f"Computer guessed ({computer_guess_row},{computer_guess_col}) and hit the battleship!")
+            if (computer_guess_row == player_ship_row) and (
+             computer_guess_col == player_ship_col):
+                print(f"Computer guessed ({computer_guess_row},{computer_guess_col})")
+                print("Computer hit your battleship!")
                 player_board[computer_guess_row][computer_guess_col] = "X"
                 print_player_board(player_board)
                 break
             elif player_board[computer_guess_row][computer_guess_col] == "-":
                 computer_guess()
             else:
-                print(f"Computer guessed ({computer_guess_row},{computer_guess_col}) and missed!")
+                print(f"Computer guessed ({computer_guess_row},{computer_guess_col})")
+                print("Computer missed your ship!")
                 print(player_ship_row, player_ship_col)
                 player_board[computer_guess_row][computer_guess_col] = "-"
                 print_player_board(player_board)
                 break
-        
+
     # Create coordinates for player ships and computer ships
 
     player_ship_row = rand_row_computer(computer_board)
@@ -110,16 +109,19 @@ while True:
             try:
                 guess_row = int(input("Guess the Row: "))
                 guess_col = int(input("Guess the Column: "))
-                if guess_row == computer_ship_row and guess_col == computer_ship_col:
+                if (guess_row == computer_ship_row) and (
+                guess_col == computer_ship_col):
                     print("You hit the battleship!")
                     computer_board[guess_row][guess_col] = "X"
                 else:
-                    if guess_row > int(board_size - 1) or guess_col > int(board_size - 1):
+                    if (guess_row > int(board_size - 1)) or (
+                    guess_col > int(board_size - 1)):
                         print("invalid guess")
                     elif computer_board[guess_row][guess_col] == "-":
                         print("You have already guessed here, try again")
-                    else: 
-                        print(f"You guessed ({guess_row},{guess_col}) and you missed!")
+                    else:
+                        print(f"You guessed ({guess_row},{guess_col})")
+                        print("You missed!")
                         print(computer_ship_row, computer_ship_col)
                         computer_board[guess_row][guess_col] = "-"
                         computer_guess()
@@ -132,8 +134,8 @@ while True:
         print(f"You sunk the battleship! Well done {name}!")
 
     # Restart the game or not
-
-    restart = input("Game Over, would you like to play again or close the game? Enter 'Y' to restart ")
+    print("Game Over, would you like to play again or close the game?")
+    restart = input("Enter 'Y' to restart ")
 
     if restart != "Y":
         break
