401830a0a5821267653c2e326608d3d0132ca56b
NRCarver <carver1994@hotmail.com>
2022-08-03 10:20:42 +0000

    Add random coordinate generator for computer ships

--- run.py
+++ run.py
@@ -1,3 +1,5 @@
+from random import randint
+
 #create board for gameplay
 
 board = []
@@ -5,8 +7,27 @@ board = []
 for i in range(0, 6):
     board.append(["O"] * 6)
 
+
 def print_board(board):
     for row in board:
         print(" ".join(row))
 
+
 print_board(board)
+
+
+#create random ship placement
+
+
+def rand_row(board):
+    print(randint(0, len(board) - 1))
+
+
+def rand_col(board):
+    print(randint(0, len(board) - 1))
+
+
+rand_row(board)
+
+
+rand_col(board)
